72c1397ce263161afcf4842d7c99b7e85512913b
tsedeniyafiseha <tsedeniya6@gmail.com>
2026-02-18 01:57:48 -0800

    Time: 270 ms (28.66%), Space: 12.8 MB (67.07%) - LeetHub

---
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -0,0 +1,14 @@
+from collections import Counter
+
+class Solution(object):
+    def minSteps(self, s, t):
+        count_s = Counter(s)
+        count_t = Counter(t)
+        
+        steps = 0
+        
+        for char in count_s:
+            if count_s[char] > count_t[char]:
+                steps += count_s[char] - count_t[char]
+        
+        return steps
