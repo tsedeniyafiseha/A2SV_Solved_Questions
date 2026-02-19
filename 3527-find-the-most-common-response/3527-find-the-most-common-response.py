9177b921c521d5aa37deca3aae951a6c016d2d92
tsedeniyafiseha <tsedeniya6@gmail.com>
2026-02-19 05:11:47 -0800

    Time: 645 ms (57.78%), Space: 179.8 MB (75.56%) - LeetHub

---
+++ 3527-find-the-most-common-response/3527-find-the-most-common-response.py
@@ -0,0 +1,20 @@
+from collections import Counter
+
+class Solution(object):
+    def findCommonResponse(self, responses):
+        count = Counter()
+
+        for person in responses:
+            unique = set(person)   # remove duplicates per list
+            for word in unique:
+                count[word] += 1
+
+        max_freq = max(count.values())
+
+        result = ""
+        for word in count:
+            if count[word] == max_freq:
+                if result == "" or word < result:
+                    result = word
+
+        return result
