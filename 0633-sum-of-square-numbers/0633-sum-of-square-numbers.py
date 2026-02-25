ea0693f717d6ebf830c41ac46316594fe38bb83a
tsedeniyafiseha <tsedeniya6@gmail.com>
2026-02-25 06:55:47 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -0,0 +1,12 @@
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        start  = 0
+        end = int(math.isqrt(c))
+        while start <= end:
+            if (start **2 + end **2) > c:
+                end -= 1
+            elif (start **2 + end **2) < c:
+                start += 1
+            else:
+                return True
+        return False
