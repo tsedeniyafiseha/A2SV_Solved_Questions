d2ebb757b1c29c3764cf860565b04945ceb550ff
tsedeniyafiseha <tsedeniya6@gmail.com>
2026-03-17 02:29:10 -0700

    Time: 99 ms (64.1%), Space: 31.2 MB (29.74%) - LeetHub

---
+++ 0456-132-pattern/0456-132-pattern.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def find132pattern(self, nums):
+
+        stack = []
+        second = float('-inf') 
+        for i in range(len(nums)-1, -1, -1):
+
+            if nums[i] < second:
+                return True
+
+            while stack and nums[i] > stack[-1]:
+                second = stack.pop()
+
+            stack.append(nums[i])
+
+        return False
