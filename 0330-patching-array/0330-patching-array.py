3d3fae7f666d2ac96904d6f3651401cf278ce112
tsedeniyafiseha <tsedeniya6@gmail.com>
2026-03-26 08:25:03 -0700

    Time: 0 ms (100%), Space: 12.5 MB (31.76%) - LeetHub

---
+++ 0330-patching-array/0330-patching-array.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def minPatches(self, nums, n):
+        """
+        :type nums: List[int]
+        :type n: int
+        :rtype: int
+        """
+        
+   
+        miss = 1
+        i = 0
+        patches = 0
+        
+        while miss <= n:
+            if i < len(nums) and nums[i] <= miss:
+                miss += nums[i]
+                i += 1
+            else:
+                miss += miss
+                patches += 1
+                
+        return patches
+        
