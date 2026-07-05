48ddc0ff9b6c9f09110fdb00fa60a815bd3c7528
tsedeniyafiseha <tsedeniya6@gmail.com>
2026-07-05 02:39:24 -0700

    Time: 0 ms (100%), Space: 19.1 MB (92.6%) - LeetHub - Time: O(n + m), Space: O(m)

--- 1893-check-if-all-the-integers-in-a-range-are-covered/1893-check-if-all-the-integers-in-a-range-are-covered.py
+++ 1893-check-if-all-the-integers-in-a-range-are-covered/1893-check-if-all-the-integers-in-a-range-are-covered.py
@@ -1,13 +1,17 @@
-class Solution(object):
-    def isCovered(self, ranges, left, right):
-        covered = set()
-        
-        for start, end in ranges:
-            for x in range(start, end + 1):
-                covered.add(x)
-        
-        for num in range(left, right + 1):
-            if num not in covered:
-                return False
-        
-        return True
+class Solution:
+    def isCovered(self, ranges: List[List[int]], left: int, right: int) -> bool:
+        diff= [0]*52
+        for start , end in ranges:
+            diff[start]+=1
+            diff[end+1]-=1
+        coverage=0
+        for i in range(1,52):
+            coverage+= diff[i]
+            if left<= i<= right and coverage==0:
+                return False 
+        return True 
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
